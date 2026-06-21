8bc251ea8346d8213c810ce097e89e0d642616ad
samman41 <165357681+samman41@users.noreply.github.com>
2026-06-21 22:15:45 +0545

    Rubric  JSON.py

--- Rubric  JSON.py
+++ Rubric  JSON.py
@@ -1,1 +1,11 @@
-
+{
+  "min_words": 150,
+  "max_words": 800,
+  "required_sections": [
+    "Introduction",
+    "Body",
+    "Conclusion"
+  ],
+  "min_citations": 1,
+  "similarity_threshold": 0.35
+}
